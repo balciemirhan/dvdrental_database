58abe2aa8a89135c1d025f856d4cb7cb8a3f9b9a
Emirhan balci <balciemirhan@hotmail.com>
2022-11-21 01:59:42 +0300

    LIMIT-OFFSET | new data

---
+++ LIMIT-OFFSET.sql
@@ -0,0 +1,23 @@
+1- film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+
+SELECT * FROM film
+WHERE title LIKE '%n'
+order by length DESC
+LIMIT 5;
+
+2- film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+
+SELECT	* from film
+WHERE title ~~ '%n'
+order by length ASC
+OFFSET 5
+LIMIT 5;
+
+3- customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+
+select * from customer
+WHERE store_id = 1
+order by last_name DESC
+LIMIT 4;
+
+
